3c2cc6976083f356038f384a16ae9f48f222c460
HenningOtte <HenningOtte91@.com>
2025-06-14 17:12:15 +0200

    'seachForEvolution' and 'getIdForEvolution' Functions - was missing in the code.

--- scripts/searchFunctions.js
+++ scripts/searchFunctions.js
@@ -40,16 +40,35 @@ function checkAndAddPokemon(id) {
         }
     }
     return false;
-}
+};
 
 function pushPokemonIfMaxID(pokemonObj) {
     if (pokemonObj.id <= amountOfCards.maxIDs) {
         addToVisible(pokemonObj);
     }
-}
+};
 
 function addToVisible(pokemon) {
     if (!visiblePokemon.some(p => p.id === pokemon.id)) {
         visiblePokemon.push(pokemon);
     }    
+};
+
+async function searchForEvolution(pokename) {
+    const searchResult = getIdForEvolution(pokename);
+    if (searchResult.length > 0) {
+        await searchInDetails(searchResult);
+    }
+};
+
+function getIdForEvolution(pokename) {
+    const name = pokename.trim().toLowerCase();
+    const result = [];
+    for (let i = 0; i < pokemonIndex.length; i++) {
+        const currentName = pokemonIndex[i].name.toLowerCase();
+        if (currentName.includes(name)) {
+            result.push(pokemonIndex[i].id);
+        }
+    }
+    return result;
 };
